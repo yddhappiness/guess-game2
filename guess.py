37f647b82d5142f61c06f6e3d65b50f3aa24b831
young <yyjdom@gmail.com>
2018-01-22 23:14:26 +0800

    some change

--- guess.py
+++ guess.py
@@ -1,10 +1,12 @@
 from random import randint
 i = randint(1,10)
-while True:
+count = 0
+while count<5:
     guess = input("What is your guess number is between 1 and 10?")
+    count += 1
     if int(guess) == i:
         print("good job")
         break
     else:
-        print("go on")
+        print("you have {} times left.".format(5-count))
         
